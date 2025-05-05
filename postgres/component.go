1e3d990d16fe3b5829b3570646ea49c00be76c5c
www-xu <wx6_29@outlook.com>
2025-05-05 18:45:30 +0800

    feat: postgres component

---
+++ postgres/component.go
@@ -0,0 +1,90 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/www-xu/spark"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Component struct {
+	ctx      *spark.ApplicationContext
+	config   *Config
+	instance *gorm.DB
+}
+
+func NewComponent() *Component {
+	return &Component{}
+}
+
+var instance *Component
+
+func init() {
+	instance = NewComponent()
+
+	spark.RegisterApplicationInitEventListener(instance)
+	spark.RegisterApplicationStopEventListener(instance)
+}
+
+func (c *Component) Instantiate() error {
+	err := c.ctx.UnmarshalKey("postgres", &c.config)
+	if err != nil {
+		return err
+	}
+
+	if c.config == nil {
+		return errors.New("postgres config isn't found")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
+		c.config.Host,
+		c.config.User,
+		c.config.Password,
+		c.config.DBName,
+		c.config.Port,
+		c.config.SSLMode,
+	)
+
+	c.instance, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Get(ctx context.Context) *gorm.DB {
+	return instance.Get(ctx)
+}
+
+func (c *Component) Get(ctx context.Context) *gorm.DB {
+	return c.instance
+}
+
+func (c *Component) Close() error {
+
+	return nil
+}
+
+func (c *Component) BeforeInit() error {
+	return nil
+}
+
+func (c *Component) AfterInit(applicationContext *spark.ApplicationContext) error {
+	c.ctx = applicationContext
+
+	return c.Instantiate()
+}
+
+func (c *Component) BeforeStop() {
+	return
+}
+
+func (c *Component) AfterStop() {
+	_ = c.Close()
+
+	return
+}
---
+++ postgres/config.go
@@ -0,0 +1,13 @@
+package postgres
+
+type Config struct {
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	Host         string `mapstructure:"host"`
+	Port         string `mapstructure:"port"`
+	DBName       string `mapstructure:"db_name"`
+	SSLMode      string `mapstructure:"ssl_mode"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+	MaxLifetime  int    `mapstructure:"max_life_time"`
+}
